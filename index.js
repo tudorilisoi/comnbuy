cb6fd8556f4172e2c08804d76755ca3444d32492
Rashad Bayram <rashadbayram@gmail.com>
2018-08-12 21:17:24 -0600

    'bestbuy search wrks walmart is not yet'

--- index.js
+++ index.js
@@ -1,48 +1,73 @@
-const bestBuySearch = 'https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))pageSize=30?format=json&apiKey=pwMuSpdnoeB0ZkCAuqlxJGuI';
+const bestBuySearch1 = 'https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=pwMuSpdnoeB0ZkCAuqlxJGuI&pageSize=10&format=json';
 const walMartSearch = 'https://api.walmartlabs.com/v1/search?';
 
 let search = "";
 let searchTerm = "";
 
-function getDataFromApi(callback) {
+function getDataFromApi() {
+ //alert('Are we getting data at all ?');
+
   let searchUrl = "";
   let jsonDataType = "";
 
-  if (search == "bestbuy"){
-    searchUrl = bestBuySearch;
+  if (search == "bestbuy") {
+    searchUrl = bestBuySearch1;
     jsonDataType = 'json';
-  } 
+  }
   else {
     searchUrl = walMartSearch + 'query=' + searchTerm + '&format=json&apiKey=9puezpg9ppwzytwy7grqvr64';
     jsonDataType = 'jsonp';
-  } 
+  }
+  //console.log('searchUrl =' + searchUrl);
+  //console.log('jsonDataType =' + jsonDataType);
+  //console.log('searchTerm =' + searchTerm);
 
   const settings = {
     url: searchUrl,
-    data: {  
-        q: `${searchTerm} in:name`,
+    data: {
+      q: `${searchTerm} in:name`,
     },
     dataType: jsonDataType,
     type: 'GET',
-    success: callback
+    success: displaySearchData,
+    error: handleerror 
+    
   };
-  $.ajax(settings);
+  $.ajax({
+    url: searchUrl,
+    data: {
+      q: `${searchTerm}`,
+    },
+    dataType: jsonDataType,
+    type: 'GET',
+    success: displaySearchData,
+    error: handleerror 
+    
+  });
 }
 
-//amend it
+function handleerror () {
+  //alert('we had an error');
+}
+
 function renderResult(result) {
-    if (search == "bestbuy") {
 
-    } 
-  //console.log(result)
-  return `
-   
-  `;
+  if (search == "bestbuy") {
+    return `
+            <li class="comparison-item">
+              ${result.index}, Rating: ${result.customerReviewCount}, Model: ${result.modelNumber}, Reviews: ${result.customerReviewAverage}, Regular price:${result.regularPrice}, Sale Price: ${result.salePrice}
+            </li>`
+  }
+  else {
+    `
+            <li class="comparison-item">
+              ${result.index}, Rating: ${result.cutomerRating}, Model: ${result.modelNumber}, Reviews: ${results.numReviews}, Regular price:${result.items}, Sale Price: ${results.salePrice}
+            </li>`
+  }
 }
-//amend it
+
 function displaySearchData(data) {
-  //console.log(data);// just to understand the data structure. delete it later
-  const results = data.items.map((item, index) => renderResult(item));
+  const results = data.products.map((products, index) => renderResult(products));
   $('.js-search-results').html(results);
 }
 
@@ -50,7 +75,7 @@ function watchSubmit() {
   $('.js-searchBestbuy-form').submit(event => {
     event.preventDefault();
     search = "bestbuy";
-    const queryTarget = $(event.currentTarget).find('.js-query');
+    const queryTarget = $(event.currentTarget).find('#site-search');
     searchTerm = queryTarget.val();
     // clear out the input
     queryTarget.val("");
@@ -60,7 +85,7 @@ function watchSubmit() {
   $('.js-searchWalmart-form').submit(event => {
     event.preventDefault();
     search = "walmart";
-    const queryTarget = $(event.currentTarget).find('.js-query');
+    const queryTarget = $(event.currentTarget).find('#site-search');
     searchTerm = queryTarget.val();
     // clear out the input
     queryTarget.val("");
