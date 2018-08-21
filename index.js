632aa4fa6fe994f14f0a516700860648bcd3b211
Rashad Bayram <rashadbayram@gmail.com>
2018-08-21 08:22:17 -0400

    'app status before pagination and filtered search is implemented''

--- index.js
+++ index.js
@@ -1,79 +1,98 @@
-const bestBuySearch1 = 'https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=pwMuSpdnoeB0ZkCAuqlxJGuI&pageSize=10&format=json';
+const bestBuySearch = 'https://api.bestbuy.com/v1/products';
 const walMartSearch = 'https://api.walmartlabs.com/v1/search?';
+
+//http://api.walmartlabs.com/v1/search?apiKey={apiKey}&query=laptop&facet=on
+
 
 let search = "";
 let searchTerm = "";
 
+
+//const STORE = {walMartSearch}
+
+
 function getDataFromApi() {
- //alert('Are we getting data at all ?');
+  //alert('Are we getting data at all ?');
 
   let searchUrl = "";
   let jsonDataType = "";
 
+
   if (search == "bestbuy") {
-    searchUrl = bestBuySearch1;
+    searchUrl = bestBuySearch + "(search=" + searchTerm + ")";
     jsonDataType = 'json';
   }
   else {
     searchUrl = walMartSearch + 'query=' + searchTerm + '&format=json&apiKey=9puezpg9ppwzytwy7grqvr64';
     jsonDataType = 'jsonp';
   }
-  //console.log('searchUrl =' + searchUrl);
-  //console.log('jsonDataType =' + jsonDataType);
-  //console.log('searchTerm =' + searchTerm);
+
 
   const settings = {
     url: searchUrl,
     data: {
-      q: `${searchTerm} in:name`,
+      format: 'json',
+      apiKey: 'pwMuSpdnoeB0ZkCAuqlxJGuI',
+      show: 'sku,name,salePrice',
+      //query: 'laptop',
     },
     dataType: jsonDataType,
     type: 'GET',
     success: displaySearchData,
-    error: handleerror 
-    
+    error: handleerror
   };
-  $.ajax({
-    url: searchUrl,
-    data: {
-      q: `${searchTerm}`,
-    },
-    dataType: jsonDataType,
-    type: 'GET',
-    success: displaySearchData,
-    error: handleerror 
-    
-  });
+  $.ajax(settings);
 }
 
-function handleerror () {
-  //alert('we had an error');
+function handleerror() {
+  alert('we had an error');
 }
 
 function renderResult(result) {
 
   if (search == "bestbuy") {
-    return `
+    return `<p> ${result.name} </p>
             <li class="comparison-item">
-              ${result.index}, Rating: ${result.customerReviewCount}, Model: ${result.modelNumber}, Reviews: ${result.customerReviewAverage}, Regular price:${result.regularPrice}, Sale Price: ${result.salePrice}
+              <ul class="resultcolumn">
+                <li> Rating: ${result.customerReviewCount}</li>
+                <li> Reviews: ${result.customerReviewAverage}</li>
+              </ul>
+              <ul class="resultcolumn">
+                <li> Regular price:${result.regularPrice}</li>
+                <li> Sale Price: ${result.salePrice}</li>
+              </ul>
             </li>`
   }
   else {
-    `
+    return `<p> ${result.name} </p>
             <li class="comparison-item">
-              ${result.index}, Rating: ${result.cutomerRating}, Model: ${result.modelNumber}, Reviews: ${results.numReviews}, Regular price:${result.items}, Sale Price: ${results.salePrice}
+              <ul class="resultcolumn">
+                <li> Rating: ${result.customerRating}</li>
+                <li> Reviews: ${result.numReviews}</li>
+              </ul>
+              <ul class="resultcolumn">
+                <li> Regular price:${result.items}</li>
+                <li> Sale Price: ${result.salePrice}</li>
+
+              </ul>
             </li>`
   }
 }
 
 function displaySearchData(data) {
-  const results = data.products.map((products, index) => renderResult(products));
-  $('.js-search-results').html(results);
+  if (search == 'bestbuy') {
+    const results = data.products.map((product) => renderResult(product));
+    $('.bestbuycomparisonlist').append(results);
+  } else {
+    const results = data.items.map((item) => renderResult(item));
+    $('.walmartcomparisonlist').append(results);
+  }
 }
 
 function watchSubmit() {
   $('.js-searchBestbuy-form').submit(event => {
     event.preventDefault();
+    $(".bestbuycomparisonlist").empty();
     search = "bestbuy";
     const queryTarget = $(event.currentTarget).find('#site-search');
     searchTerm = queryTarget.val();
@@ -84,6 +103,7 @@ function watchSubmit() {
 
   $('.js-searchWalmart-form').submit(event => {
     event.preventDefault();
+    $(".walmartcomparisonlist").empty();
     search = "walmart";
     const queryTarget = $(event.currentTarget).find('#site-search');
     searchTerm = queryTarget.val();
@@ -93,5 +113,32 @@ function watchSubmit() {
   });
 }
 
-
 $(watchSubmit);
+
+
+/* ADDING SMOOTH SCROLLING TO PAGES*/
+
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
